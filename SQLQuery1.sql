58d593ba9a772d82acf3c86205e1f345885e0f93
thegig009 <loveesesgig2005@gmail.com>
2025-08-07 09:50:15 +0700

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,25 @@
-select * from products
-where price = 15
+SELECT * FROM products where Unitprice = 15
+
+SELECT * FROM products where UnitsInStock < 250
+
+SELECT * FROM products where Discontinued = 1
+
+SELECT ProductID, ProductName, Unitprice
+FROM Products
+WHERE Unitprice > 100
+
+SELECT ProductID, (Unitprice * UnitsInStock) AS TotalPrice
+FROM Products
+
+SELECT ReceiptID, ReceiptDate, TotalCash
+FROM Receipts
+WHERE ReceiptDate < '2025-02-15'
+
+SELECT ProductID, ProductName
+FROM Products
+WHERE  UnitsInStock >= 400
+
+SELECT ProductID, ProductName, Unitprice, UnitsInStock
+FROM Products
+WHERE ProductName IN ('แชมพู', 'แป้งเด็ก', 'สบู่', 'ยาสีฟัน');
+
